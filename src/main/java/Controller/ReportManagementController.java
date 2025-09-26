a4b592fee8f9eb0c3d59eef4dec101742d710a12
matheeshapr <matheeshap62@gmail.com>
2025-09-26 11:43:27 +0530

    fix: correct spelling errors in UI labels

--- src/main/java/Controller/ReportManagementController.java
+++ src/main/java/Controller/ReportManagementController.java
@@ -1,9 +1,6 @@
 package Controller;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class ReportManagementController {
 
