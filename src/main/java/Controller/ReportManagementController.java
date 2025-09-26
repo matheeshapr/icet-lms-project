b0b7a9e6c6bb1f7f30fc4b5e9c192f8a78caffff
matheeshapr <matheeshap62@gmail.com>
2025-09-26 11:57:14 +0530

    refactor: update button actions and labels in report management UI

--- src/main/java/Controller/ReportManagementController.java
+++ src/main/java/Controller/ReportManagementController.java
@@ -4,13 +4,4 @@ import javafx.event.ActionEvent;
 
 public class ReportManagementController {
 
-    public void btnindtrain(ActionEvent actionEvent) {
-
-    }
-
-    public void btnbatchwise(ActionEvent actionEvent) {
-    }
-
-    public void btnstureg(ActionEvent actionEvent) {
-    }
 }
